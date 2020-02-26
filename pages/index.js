c274bc79c60051734cfbb136898e572d98a3707e
Badal Moradia <badal@telnyx.com>
2020-02-26 11:50:04 -0600

    Try to implement polling

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import fetch from 'isomorphic-unfetch'
+import React from 'react';
+import ReactPolling from 'react-polling';
 
 const Home = ({ words, resp }) => (
 
@@ -10,44 +12,94 @@ const Home = ({ words, resp }) => (
     </Head>
 
     <main>
-      <h1 className="title">
-        {words}
-      </h1>
 
-      {resp}
+      <ReactPolling
+        url={'http://faker.hook.io/'}
+        interval= {3000} // in milliseconds(ms)
+        // retryCount={3} // this is optional
+        onSuccess={(resp) => {
+          console.log(resp.text())
+          return true
+        }}
+        onFailure={() => console.log('handle failure')} // this is optional
+        method={'GET'}
+        render={({ startPolling, stopPolling, isPolling }) => {
+          console.log("rendering")
+          console.log(isPolling)
+          // startPolling()
+          console.log(stopPolling)
+          // renderPuzzle()
+          return <p>This</p>
 
 
+          }}
+        />
 
     </main>
   </div>
 )
 
 
-Home.getInitialProps = async ({ req }) => {
-  // const res = await fetch('https://api.github.com/repos/zeit/next.js')
-  // const json = await res.json()
+// Home.getInitialProps = async ({ req }) => {
 
-  const json = {words: [["b", , , "n", ], [, "o", ], [, , ], [, , , , , ]]}
+//   // const res = await fetch('http://faker.hook.io/')
+//   // let json = await res.text()
 
-  let words = json.words
+//   // console.log(json)
 
+//   // json = {words: [["b", , , "n", ], [, "o", ], [, , ], [, , , , , ]]}
+
+//   // let words = json.slice(1, -2).split(" ")
+
+//   // let render = renderPuzzle(words)
+
+//   return {words: <ReactPolling
+//     url={'http://faker.hook.io/'}
+//     interval= {3000} // in milliseconds(ms)
+//     // retryCount={3} // this is optional
+//     onSuccess={() => {
+//       // console.log(resp.text())
+//       return true
+//     }}
+//     onFailure={() => console.log('handle failure')} // this is optional
+//     method={'GET'}
+//     render={({ startPolling, stopPolling, isPolling }) => {
+//       console.log("rendering")
+//       console.log(isPolling)
+//       // startPolling()
+//       console.log(stopPolling)
+//       // renderPuzzle()
+//       return <p>This</p>
+
+
+//     }}
+//   />
+//   }
+// }
+
+
+const renderPuzzle = (words) => {
   let resp = []
-
   for(let i = 0; i < words.length; i++) {
     let word = words[i]
     let container = []
     for(let j = 0; j <= word.length; j++) {
       if(word[j] === "" || word[j] === undefined) {
-        container.push(<span> _ </span>)
+        container.push(<span key={j}> _ </span>)
       } else {
-        container.push(<span> {word[j]} </span>)
+        container.push(<span key={j}> {word[j]} </span>)
       }
     }
     resp.push(container)
     resp.push(<br></br>)
   }
 
-  return { words: words, resp: resp }
+  return resp
+}
+
+
+const sleep = async () => {
+  await sleep(2000)
 }
 
 export default Home
