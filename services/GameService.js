bb8e0dfc54e5b3585a122a8d7e50700c19f0892d
Hugo Bessa <eu@hugobessa.com.br>
2020-02-26 21:09:04 -0300

    clean up, docs

--- services/GameService.js
+++ services/GameService.js
@@ -4,20 +4,6 @@ const GameService = {
   /**
    * example response {words: [[b,r,i,n,g], [y,o,u,r], [o,w,n], [c,a,r,r,i,e,r]]}
    */
-  // postSession() {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve({
-  //         words: [
-  //           ["b", undefined, undefined, "n", undefined],
-  //           [undefined, "o", undefined, undefined],
-  //           [undefined, undefined, undefined],
-  //           [undefined, undefined, undefined, undefined, undefined, undefined]
-  //         ]
-  //       });
-  //     }, 150);
-  //   });
-  // },
 
   postSession() {
     return axios.post("https://ef3df964.ngrok.io/data/letter").then(
@@ -51,19 +37,6 @@ const GameService = {
    *   "lastWinner": "Winner Alias"
    * }
    */
-  // getMetadata() {
-  //   return new Promise((resolve, reject) => {
-  //     setTimeout(() => {
-  //       resolve({
-  //         sessionActive: true,
-  //         leaderboard: [{ "blue monkey": 12 }],
-  //         roundNumber: 3,
-  //         number: "+18004005000",
-  //         lastWinner: "Winner Alias"
-  //       });
-  //     }, 150);
-  //   });
-  // }
 
   getMetadata() {
     return axios
