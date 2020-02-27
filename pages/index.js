07ee23ff5477b76744d0156eb281cfee1988b7aa
Hugo Bessa <eu@hugobessa.com.br>
2020-02-27 09:09:49 -0300

    Improve layout and add animation

--- pages/index.js
+++ pages/index.js
@@ -24,6 +24,7 @@ const Home = ({ words, resp }) => {
   function fetchMetadata() {
     console.log("fetchMetadata");
     return GameService.getMetadata().then(response => {
+      console.log(response);
       setGameState({ meta: response });
     });
   }
@@ -31,6 +32,7 @@ const Home = ({ words, resp }) => {
   function fetchSession() {
     console.log("fetchSession");
     return GameService.postSession().then(session => {
+      console.log(session);
       setGameState({ session });
     });
   }
@@ -66,8 +68,15 @@ const Home = ({ words, resp }) => {
       <Head>
         <title>Wheel of Fortune</title>
         <link
-          href="https://fonts.googleapis.com/css?family=Bungee|Roboto&display=swap"
+          href="https://fonts.googleapis.com/css?family=Roboto+Mono&display=swap"
           rel="stylesheet"
+        />
+        <style
+          children={`
+              body, html { margin: 0; }
+              body { background-color: #1D2241; }
+              * { box-sizing: border-box; }
+            `}
         />
       </Head>
       <main>
--- services/GameService.js
+++ services/GameService.js
@@ -6,12 +6,20 @@ const GameService = {
    */
 
   postSession() {
-    return axios.post("https://ef3df964.ngrok.io/data/letter").then(
-      response =>
-        console.log(response) || {
-          words: response.data.phrase
-        }
-    );
+    return Promise.resolve({
+      words: [
+        ["b", "-", "-", "n", "g"],
+        ["y", "o", "u", "r"],
+        ["o", "w", "-"],
+        ["c", "a", "r", "r", "i", "e", "r"]
+      ]
+    });
+    // return axios.post("https://ef3df964.ngrok.io/data/letter").then(
+    //   response =>
+    //     console.log(response) || {
+    //       words: response.data.phrase
+    //     }
+    // );
   },
 
   /**
@@ -39,9 +47,20 @@ const GameService = {
    */
 
   getMetadata() {
-    return axios
-      .get("https://ef3df964.ngrok.io/data/meta")
-      .then(response => response.data);
+    return Promise.resolve({
+      winners: [
+        { phoneNumber: "+5513997598554" },
+        { phoneNumber: "+5513997598534" }
+      ],
+      sessionActive: true,
+      leaderboard: [{ "blue monkey": 12 }],
+      roundNumber: 3,
+      number: "+18004005000",
+      lastWinner: "Winner Alias"
+    });
+    // return axios
+    //   .get("https://ef3df964.ngrok.io/data/meta")
+    //   .then(response => response.data);
   }
 };
 
